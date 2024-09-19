d8e54d9196a9eea50452142d951575e2a7fad6a2
David Hernández Giménez <davidhernandez@ceroca.cat>
2024-09-19 19:31:13 +0200

    movimiento a la nave

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -6,10 +6,21 @@ public class NauJugador : MonoBehaviour
 {
     private float vel;
 
+    private Vector2 minPantalla;
+    private Vector2 maxPantalla;
+
     // Start is called before the first frame update
     void Start()
     {
         vel = 8;
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        minPantalla.x = (float)(minPantalla.x + 0.35);
+        minPantalla.y = (float)(minPantalla.y + 0.35);
+
+        maxPantalla.x = (float)(minPantalla.x + 0.35);
+        maxPantalla.y = (float)(minPantalla.y + 0.35);
     }
 
     // Update is called once per frame
@@ -25,6 +36,9 @@ public class NauJugador : MonoBehaviour
         Vector2 nuevaPosi = transform.position;
 
         nuevaPosi = nuevaPosi + direcionIndicada * vel * Time.deltaTime;
+
+        nuevaPosi.x = Mathf.Clamp(nuevaPosi.x, minPantalla.x, maxPantalla.x);
+        nuevaPosi.y = Mathf.Clamp(nuevaPosi.y, minPantalla.y, maxPantalla.y);
 
         transform.position = nuevaPosi;
     }
