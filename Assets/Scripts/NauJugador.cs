a4b6080ce62a4f0c8db11218e48940bdd9773883
David Hernández Giménez <davidhernandez@ceroca.cat>
2024-09-30 16:32:46 +0200

    disparos

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
 
@@ -9,6 +11,9 @@ public class NauJugador : MonoBehaviour
     private Vector2 minPantalla;
     private Vector2 maxPantalla;
 
+    [SerializeField] private GameObject PrfabProyectil;
+
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +21,9 @@ public class NauJugador : MonoBehaviour
         minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
         maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
 
-        float TamNaveX = GetComponent<SpriteRenderer>().sprite.bounds.size.x * transform.localScale.x;
-        float TamNaveY = GetComponent<SpriteRenderer>().sprite.bounds.size.y / 2;
+        //float TamNaveY = GetComponent<SpriteRenderer>().sprite.bounds.size.y / 2;
+        float TamNaveX = GetComponent<SpriteRenderer>().sprite.bounds.size.x * transform.localScale.x /2;
+        float TamNaveY = GetComponent<SpriteRenderer>().sprite.bounds.size.y * transform.localScale.y /2;
 
         //minPantalla.x = (float)(minPantalla.x + 0.75);
         minPantalla.x += TamNaveX;
@@ -29,12 +35,29 @@ public class NauJugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovimientoNave();
+        DisparaProyectil();
+    }
+
+    private void DisparaProyectil()
+    {
+        if (Input.GetKeyDown("space")) { 
+        
+            GameObject Proyectil = Instantiate(PrfabProyectil);
+            
+            Proyectil.transform.position = transform.position;
+        }
+    }
+
+    private void MovimientoNave()
+    {
+        
         float direcionIndicadaX = Input.GetAxisRaw("Horizontal");
         float direcionIndicadaY = Input.GetAxisRaw("Vertical");
 
         //Debug.Log("X: " + direcionIndicadaX + "Y: " + direcionIndicadaY);
 
-        Vector2 direcionIndicada = new Vector2(direcionIndicadaX,direcionIndicadaY).normalized;
+        Vector2 direcionIndicada = new Vector2(direcionIndicadaX, direcionIndicadaY).normalized;
 
         Vector2 nuevaPosi = transform.position;
 
@@ -46,3 +69,4 @@ public class NauJugador : MonoBehaviour
         transform.position = nuevaPosi;
     }
 }
+    
---
+++ Assets/Scripts/ProyectilJugador.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProyectilJugador : MonoBehaviour
+{
+    [SerializeField] private float vel;
+
+    private Vector2 maxpantalla;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vel = 13;
+        maxpantalla = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 posAct = transform.position;
+        posAct = posAct + new Vector2(0, 1) * vel * Time.deltaTime;
+        transform.position = posAct;
+
+        if (transform.position.y > maxpantalla.y){
+        Destroy(gameObject);
+        }
+    }
+}
