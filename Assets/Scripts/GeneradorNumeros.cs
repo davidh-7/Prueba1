43ffc1adfbbbd9c7573384e1fbd8f82f8322302a
David Hernández Giménez <davidhernandez@ceroca.cat>
2024-10-03 17:03:48 +0200

    generador de enemigos

--- Assets/Scripts/GeneradorNumeros.cs
+++ Assets/Scripts/GeneradorNumeros.cs
@@ -9,9 +9,24 @@ public class GeneradorNumeros : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        InvokeRepeating("generarNumero", 1f, 2f);
         
     }
 
+    private void InvokeRepeating() { 
+    }
+
+    private void generarNumero() { 
+    
+        GameObject numero = Instantiate(PrefabNumero);
+
+        numero.transform.position = new Vector2( Random.Range(minPantalla.x, maxPantalla.x) , maxPantalla.y);
+    }
+
+    private Vector2 minPantalla, maxPantalla;
     // Update is called once per frame
     void Update()
     {
